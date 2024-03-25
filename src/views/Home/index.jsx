d3ef4b884263637cee15c951372186b3d67b4e23
Mateo Ceballos <mateo.ceballos@gopass.com.co>
2024-03-24 20:15:19 -0500

    carrusel

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -6,12 +6,6 @@ import img4 from "../../assets/images/Views/index/Carrusel2.jpg";
 import img5 from "../../assets/images/Views/index/Carrusel3.jpg";
 import parraf1 from "../../assets/texts";
 export default function Index() {
-  const myCarouselElement = document.querySelector('#myCarousel')
-
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: false
-})
   return (
     <>
       <section className="SectionIndex sectionOrderImage">
@@ -45,27 +39,36 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
         </div>
       </section>
       <div id="carouselExampleFade" class="carousel slide carousel-fade">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={img3} class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={img4} class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={img5} class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={img3} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={img4} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={img5} class="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </>
-    
   );
 }
